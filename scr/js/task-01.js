7a6d09981981e161c99499edfea10a820e0a0aad
rusldrim <116371573+rusldrim@users.noreply.github.com>
2023-03-09 16:04:52 +0200

    Update task-01.js

--- scr/js/task-01.js
+++ scr/js/task-01.js
@@ -1,15 +1,6 @@
 const categories = document.querySelector('ul').children.length;
 console.log("Number of categories:", categories);
 
-
-// const ul = Array.from(document.querySelector('#categories').children);
-// for (let elem of ul) {
-//     let category = elem.firstElementChild.textContent;   
-//     let quantityElem = elem.lastElementChild.children.length;
-//     console.log(`Category: ${category}`); 
-//     console.log(`Elements: ${quantityElem}`);
-// };
-
 const items = document.querySelectorAll('.item');
 Array.prototype.forEach.call(items, (element) => {
     const title = element.querySelector('h2').innerHTML;
