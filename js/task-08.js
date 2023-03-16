0923a0086d6b880e9be7bde882dde45654ec34c2
rusldrim <116371573+rusldrim@users.noreply.github.com>
2023-03-16 09:46:02 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -12,7 +12,7 @@ function handleSubmit(event) {
     if (email.value === '' || password.value === '') {
         return alert('Please fill in all the fields!')
     }
-    const userDetails = { email: email.value, Password: password.value }
+    const userDetails = { email: email.value, password: password.value }
     
     console.log(userDetails)
     event.currentTarget.reset()
